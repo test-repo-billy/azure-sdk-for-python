79225e74181e74a9b153b0fff7073007d645fe2e
SDK Automation <sdkautomation@microsoft.com>
2019-11-01 06:24:49 +0000

    Generated from 27d5fcbffb59163e5aeda0e779ecd92fff5ae415
    add clear-output-folder in resource-manager readme.typescript.md to fix js sdk build failure

--- sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
@@ -18,6 +18,7 @@ try:
     from ._models_py3 import AmlComputeNodeInformation
     from ._models_py3 import AmlComputeNodesInformation
     from ._models_py3 import AmlComputeProperties
+    from ._models_py3 import AmlUserFeature
     from ._models_py3 import ClusterUpdateParameters
     from ._models_py3 import Compute
     from ._models_py3 import ComputeNodesInformation
@@ -40,13 +41,24 @@ try:
     from ._models_py3 import Operation
     from ._models_py3 import OperationDisplay
     from ._models_py3 import Password
+    from ._models_py3 import QuotaBaseProperties
+    from ._models_py3 import QuotaUpdateParameters
     from ._models_py3 import RegistryListCredentialsResult
     from ._models_py3 import Resource
     from ._models_py3 import ResourceId
+    from ._models_py3 import ResourceName
+    from ._models_py3 import ResourceQuota
+    from ._models_py3 import ResourceSkuLocationInfo
+    from ._models_py3 import ResourceSkuZoneDetails
+    from ._models_py3 import Restriction
     from ._models_py3 import ScaleSettings
     from ._models_py3 import ServicePrincipalCredentials
+    from ._models_py3 import Sku
+    from ._models_py3 import SKUCapability
     from ._models_py3 import SslConfiguration
     from ._models_py3 import SystemService
+    from ._models_py3 import UpdateWorkspaceQuotas
+    from ._models_py3 import UpdateWorkspaceQuotasResult
     from ._models_py3 import Usage
     from ._models_py3 import UsageName
     from ._models_py3 import UserAccountCredentials
@@ -57,6 +69,7 @@ try:
     from ._models_py3 import VirtualMachineSizeListResult
     from ._models_py3 import VirtualMachineSshCredentials
     from ._models_py3 import Workspace
+    from ._models_py3 import WorkspaceSku
     from ._models_py3 import WorkspaceUpdateParameters
 except (SyntaxError, ImportError):
     from ._models import AKS
@@ -67,6 +80,7 @@ except (SyntaxError, ImportError):
     from ._models import AmlComputeNodeInformation
     from ._models import AmlComputeNodesInformation
     from ._models import AmlComputeProperties
+    from ._models import AmlUserFeature
     from ._models import ClusterUpdateParameters
     from ._models import Compute
     from ._models import ComputeNodesInformation
@@ -89,13 +103,24 @@ except (SyntaxError, ImportError):
     from ._models import Operation
     from ._models import OperationDisplay
     from ._models import Password
+    from ._models import QuotaBaseProperties
+    from ._models import QuotaUpdateParameters
     from ._models import RegistryListCredentialsResult
     from ._models import Resource
     from ._models import ResourceId
+    from ._models import ResourceName
+    from ._models import ResourceQuota
+    from ._models import ResourceSkuLocationInfo
+    from ._models import ResourceSkuZoneDetails
+    from ._models import Restriction
     from ._models import ScaleSettings
     from ._models import ServicePrincipalCredentials
+    from ._models import Sku
+    from ._models import SKUCapability
     from ._models import SslConfiguration
     from ._models import SystemService
+    from ._models import UpdateWorkspaceQuotas
+    from ._models import UpdateWorkspaceQuotasResult
     from ._models import Usage
     from ._models import UsageName
     from ._models import UserAccountCredentials
@@ -106,18 +131,27 @@ except (SyntaxError, ImportError):
     from ._models import VirtualMachineSizeListResult
     from ._models import VirtualMachineSshCredentials
     from ._models import Workspace
+    from ._models import WorkspaceSku
     from ._models import WorkspaceUpdateParameters
+from ._paged_models import AmlUserFeaturePaged
 from ._paged_models import ComputeResourcePaged
 from ._paged_models import OperationPaged
+from ._paged_models import ResourceQuotaPaged
 from ._paged_models import UsagePaged
 from ._paged_models import WorkspacePaged
+from ._paged_models import WorkspaceSkuPaged
 from ._azure_machine_learning_workspaces_enums import (
     ProvisioningState,
     UsageUnit,
+    QuotaUnit,
+    Status,
     ResourceIdentityType,
     VmPriority,
+    RemoteLoginPortPublicAccess,
     AllocationState,
+    NodeState,
     ComputeType,
+    ReasonCode,
     UnderlyingResourceAction,
 )
 
@@ -130,6 +164,7 @@ __all__ = [
     'AmlComputeNodeInformation',
     'AmlComputeNodesInformation',
     'AmlComputeProperties',
+    'AmlUserFeature',
     'ClusterUpdateParameters',
     'Compute',
     'ComputeNodesInformation',
@@ -152,13 +187,24 @@ __all__ = [
     'Operation',
     'OperationDisplay',
     'Password',
+    'QuotaBaseProperties',
+    'QuotaUpdateParameters',
     'RegistryListCredentialsResult',
     'Resource',
     'ResourceId',
+    'ResourceName',
+    'ResourceQuota',
+    'ResourceSkuLocationInfo',
+    'ResourceSkuZoneDetails',
+    'Restriction',
     'ScaleSettings',
     'ServicePrincipalCredentials',
+    'Sku',
+    'SKUCapability',
     'SslConfiguration',
     'SystemService',
+    'UpdateWorkspaceQuotas',
+    'UpdateWorkspaceQuotasResult',
     'Usage',
     'UsageName',
     'UserAccountCredentials',
@@ -169,16 +215,25 @@ __all__ = [
     'VirtualMachineSizeListResult',
     'VirtualMachineSshCredentials',
     'Workspace',
+    'WorkspaceSku',
     'WorkspaceUpdateParameters',
     'OperationPaged',
     'WorkspacePaged',
+    'AmlUserFeaturePaged',
     'UsagePaged',
+    'ResourceQuotaPaged',
     'ComputeResourcePaged',
+    'WorkspaceSkuPaged',
     'ProvisioningState',
     'UsageUnit',
+    'QuotaUnit',
+    'Status',
     'ResourceIdentityType',
     'VmPriority',
+    'RemoteLoginPortPublicAccess',
     'AllocationState',
+    'NodeState',
     'ComputeType',
+    'ReasonCode',
     'UnderlyingResourceAction',
 ]
